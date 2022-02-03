6c7ff082fddb7db7c517cb7ff953fdf4325f014c
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-02-03 15:51:35 -0800

    Improve dev experience and remove postcss

--- index.ts
+++ index.ts
@@ -8,6 +8,21 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+app.get("/website/*", (req, res) => {
+    const p = path.join(__dirname, "public", req.path.replace("/website", ""));
+    const p2 = path.join(__dirname, "views", (req.path.replace("/website", "") == "/" ? "index" : req.path.replace("/website", "")) + ".ejs");
+    if(fs.existsSync(p) && req.path.replace("/website", "") != "/") {
+        res.sendFile(p);
+    } else {
+        if(req.path == "/website/index.html" || req.path == "/website/index.html.ejs") return res.redirect("/website");
+        if(fs.existsSync(p2)) {
+            res.render(p2);
+        } else {
+            res.render(path.join(__dirname, "views/404.ejs"));
+        }
+    }
+});
+
 app.get("/*", (req, res) => {
     var p = req.path;
     if (req.path == "/") {
