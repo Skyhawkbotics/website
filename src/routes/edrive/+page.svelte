a654c3d1fa81023d9d7bb12955e31ba61eb28717
Dillboy949 <133061550+Dillboy949@users.noreply.github.com>
2023-05-09 16:00:12 -0700

    Added electronics and minichallenge pages.

---
+++ src/routes/edrive/+page.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+    <title>Electronics Drive | Hawkbot1cs</title>
+<svelte:head>
+
+<p class="section-title">Electronics Drive</p>
+
+
+
+<style lang="scss">
+    @import "$styles/variables";
+
+    .section-title {
+        font-family: "Roboto Slab", serif;
+        font-size: 40pt;
+        font-weight: bold;
+        color: $pageText;
+        margin-left: 5rem;
+
+        &:not(.open) {
+            margin-left: 3rem;
+        }
+    }
+</style>
