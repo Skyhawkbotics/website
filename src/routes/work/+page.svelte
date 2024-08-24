f98fce0b0892e03eb52ed1144b6c8ecc38870ce4
Insignia101 <98855763+Insignia101@users.noreply.github.com>
2024-08-24 16:49:37 -0700

    Update +page.svelte

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -21,13 +21,19 @@
     <p class="about--text">
         Occupations in the STEM world will dominate our economy and its effects will be felt in our lives, whether we are directly involved or not. That’s why it’s critical to provide youth with free, quality STEM education to meet them where they are at, inspire them, and support them in their growth. The best way to learn is in an environment where cooperation and competition are balanced, where all youth are included, and where everyone grows, succeeds, and has fun! 
     </p>
+    
+    <h1>Our Mission:</h1>
     <p class="about--text">
-      Our Mission:
-      Our mission is to provide access to STEM (science, technology, education, and mathematics) education for youth in our community. Through youth participation in preparing, designing, building, testing, promoting, and competing with robots, Hawkbot1cs provides learning experiences related to STEM education, but also character development, encouraging leadership, communication, teamwork, and more.
+        Our mission is to provide access to STEM (science, technology, education, and mathematics) education for youth in our community. Through youth participation in preparing, designing, building, testing, promoting, and competing with robots, Hawkbot1cs provides learning experiences related to STEM education, but also character development, encouraging leadership, communication, teamwork, and more.
+    </p>
 
-      Our Work:
-      We currently offer STEM opportunities to youth in our community directly through our robotics team, in which students from grades 7-12 are invited to participate and anyone else is welcome to mentor and support us! We also often attend elementary and middle school science fairs in an effort to inspire youth and have displays at feeder school club fairs. For more information about current upcoming events, head over to our Events page for live updates on our events!
 
+    <h1>Our Work:</h1>
+    <p class="about--text">
+        We currently offer STEM opportunities to youth in our community directly through our robotics team, in which students from grades 7-12 are invited to participate and anyone else is welcome to mentor and support us! We also often attend elementary and middle school science fairs in an effort to inspire youth and have displays at feeder school club fairs. For more information about current upcoming events, head over to our Events page for live updates on our events!
+    </p>
+      
+      
     </p>
 </section>
 
