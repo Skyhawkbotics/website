92aa76c948159c5c2029e95d8fc5852c9ec12d73
NoSadBeHappy <95050434+NoSadBeHappy@users.noreply.github.com>
2024-08-24 18:02:47 -0700

    Update constants.ts

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,4 +1,4 @@
-export const emailLink = "mailto:main@skyhawkbotics.org?subject=I wanted to get ahold of you.";
+export const emailLink = "mailto:skyhawkbotics@gmail.com?subject=I wanted to get ahold of you.";
 export const phoneLink = "tel:971 384-0644";
 
 export const email = "main@skyhawkbotics.org";
