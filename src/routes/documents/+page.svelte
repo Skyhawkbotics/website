fd63047e34daeb9bcefb527c22b0bd4163de6d87
Insignia101 <98855763+Insignia101@users.noreply.github.com>
2024-09-15 17:01:49 -0700

    Update +page.svelte

--- src/routes/documents/+page.svelte
+++ src/routes/documents/+page.svelte
@@ -8,15 +8,23 @@
     <title>Nonprofit Documents and Financial Information</title>
 </svelte:head>
 
+  <p class="section-title" class:open={$isMenuOpen}>Overview</p>
     <p class="about--text">
         As a IRS-recognized, 501(c)(3)-classified nonprofit, we are committed to a high level of transparency and accuracy when it comes to our documents and records. Below, you will find copies of our founding documents (Articles of Incorporation, bylaws, and Form 1023-EZ), a history of our Meeting Minutes, and our most recently filed Form 990. If you would like to request any other documents, please contact <a href="mailto:skyhawkbotics@gmail.com?subject=I wanted to get ahold of you.">skyhawkbotics@gmail.com</a> and we’ll try to get back to you as soon as possible.
     </p>
 
   <p class="section-title" class:open={$isMenuOpen}>Documents</p>
-    
+  <p class="section-heading" class:open{$isMenuOpen}>Articles of Incorporation</p>  
+
+  <p class="section-heading" class:open{$isMenuOpen}>Bylaws</p>
+
+  <p class="section-heading" class:open{$isMenuOpen}>Form 1023-EZ</p>
+
+  <p class="section-heading" class:open{$isMenuOpen}>Meeting Minutes</p>
+
 
   <p class="section-title" class:open={$isMenuOpen}>Financial Records</p>
-
+<p class="about--text"> No records to display at this moment! We will update this as soon as possible. </p>
 <br /><br />
 
 <style lang="scss">
@@ -53,6 +61,16 @@
     .section-title {
         font-family: "Roboto Slab", serif;
         font-size: 40pt;
+        font-weight: bold;
+        color: $pageText;
+        margin-left: 5rem;
+
+        &:not(.open) {
+            margin-left: 3rem;
+        }
+    .section-heading {
+        font-family: "Open Sans", serif;
+        font-size: 20pt;
         font-weight: bold;
         color: $pageText;
         margin-left: 5rem;
