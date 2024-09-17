e68a4a345693569bc745ee98ccf9d471d8e35bd5
Trevor <trevor271c@protonmail.com>
2024-09-16 17:52:39 -0700

    now actually fix it idk why im still putting comments here

--- src/routes/documents/+page.svelte
+++ src/routes/documents/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <svelte:head>
-    <title>Nonprofit Documents and Financial Information</title>
+  <title>Nonprofit Documents and Financial Information</title>
 </svelte:head>
 
   <p class="section-title" class:open={$isMenuOpen}>Overview</p>
@@ -14,10 +14,10 @@
         As a IRS-recognized, 501(c)(3)-classified nonprofit, we are committed to a high level of transparency and accuracy when it comes to our documents and records. Below, you will find copies of our founding documents (Articles of Incorporation, bylaws, and Form 1023-EZ), a history of our Meeting Minutes, and our most recently filed Form 990. If you would like to request any other documents, please contact <a href="mailto:skyhawkbotics@gmail.com?subject=I wanted to get ahold of you.">skyhawkbotics@gmail.com</a> and we’ll try to get back to you as soon as possible.
   </p>
 
-  <p>class="section-title" class:open={$isMenuOpen}>Documents</p>
-  <p>class="section-heading" class:open={$isMenuOpen}>Articles of Incorporation</p>  
+  <p class="section-title" class:open={$isMenuOpen}>Documents</p>
+  <p class="section-heading" class:open={$isMenuOpen}>Articles of Incorporation</p>  
   <p>Unable to display PDF file. <a href=$assets/documents/ArticlesOfIncorporation23.pdf">Download</a> instead.</p>
-    </object>
+  </object>
 
   <p class="section-heading" class:open={$isMenuOpen}>Bylaws</p>
 
